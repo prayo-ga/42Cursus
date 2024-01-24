0e0dce4e3a6de04fbc4295564183fe5917187ef6
Paula⚡ <150956495+prayo-ga@users.noreply.github.com>
2024-01-24 12:06:25 +0100

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: prayo-ga <prayo-ga@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 14:51:42 by prayo-ga          #+#    #+#             */
+/*   Updated: 2023/12/05 14:51:44 by prayo-ga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+//Definimos un buffer size por defecto
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+void	ft_bzero(void *s, size_t n);
+void	*ft_bzero(void *s, size_t n);
+char	*ft_strjoin(const char *s1, const char *s2);
+
+#endif
